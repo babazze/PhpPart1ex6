db5b2be768cb704baef5813fa62a40efdc3fd9f1
babazze <babazze@live.fr>
2019-12-02 16:53:39 +0100

    fini

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$km = 140;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>ex3</title>
+  </head>
+  <body>
+      <p><?= ($km + 30) / 2  ?></p>
+  </body>
+</html>
